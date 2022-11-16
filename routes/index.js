b3f9ee64640a5303453e3c38d49ca04779451058
playmaker <raymondlaw432@gmail.com>
2022-11-16 20:44:32 +0800

    Complete most the route

--- routes/index.js
+++ routes/index.js
@@ -15,108 +15,106 @@ MongoClient.connect(url, function (err, client) {
 
 /* Handle the Form */
 router.post('/insert_book', async function (req, res) {
-req.body.type = "book"
+req.body.type = "Book"
 let result = await db.collection("ngoData").insertOne(req.body);
 res.redirect('index.html');
 });
 
 /* Handle the Form */
 router.post('/insert_game', async function (req, res) {
-  req.body.type = "game"
+  req.body.type = "Game"
   let result = await db.collection("ngoData").insertOne(req.body);
   res.redirect('index.html');
  });
 
-
-// Form for updating a single Booking 
-// router.get('/update/:id', async function (req, res) {
-
-//   if (!ObjectId.isValid(req.params.id))
-//     return res.status(404).send('Unable to find the requested resource!');
-
-//   let result = await db.collection("bookings").findOne({ _id: ObjectId(req.params.id) });
-
-//   if (!result) return res.status(404).send('Unable to find the requested resource!');
-
-//   var numTickets = parseInt(result.numTickets);
-
-//   res.render("update", { booking: result, numTickets: numTickets })
-
-// });
-
-// router.get('/search', async function (req, res) {
-
-// 	var whereClause = {};
-
-// 	if (req.query.email) whereClause.email = { $regex: req.query.email };
-// 	  var parsedNumTickets = parseInt(req.query.numTickets);
-//     if (!isNaN(parsedNumTickets)) whereClause.numTickets = parsedNumTickets;	
-//     if (req.query.team) whereClause.team = req.query.team;
-//     if (req.query.superhero) whereClause.superhero = req.query.superhero;
-//     if (req.query.payment) whereClause.payment = req.query.payment;
-//     if (req.query.terms) whereClause.terms = req.query.terms;
-
-//     var perPage = 6;
-
-//   	var results = await db.collection("bookings").find(whereClause, {
-//    		limit: perPage,
-//    	 	skip: perPage * (Math.max(req.query.page - 1, 0) || 0)
-//  	}).toArray();
-
-//   var curPage = parseInt(req.query.page) || 1;
-
-// 	var pages = Math.ceil(await db.collection("bookings").count() / perPage);
-
-// 	return res.render('paginate', { bookings: results, pages: pages, perPage: perPage, email: whereClause.email, numTickets: whereClause.numTickets, team: whereClause.team, superhero: whereClause.superhero, payment: whereClause.payment, terms:whereClause.terms, currentPage: curPage});
-// });
-
-// Updating a single Booking 
-// router.post('/update/:id', async function (req, res) {
-
-//   if (!ObjectId.isValid(req.params.id))
-//     return res.status(404).send('Unable to find the requested resource!');
-
-//   req.body.numTickets = parseInt(req.body.numTickets);
-
-//   var result = await db.collection("bookings").findOneAndReplace({ _id: ObjectId(req.params.id) },
-//     req.body
-//   );
-
-//   if (!result.value)
-//     return res.status(404).send('Unable to find the requested resource!');
-
-//   res.render("update_redirect");
-// });
-
-// Delete a single Booking (I remove the original one as )
-// router.get('/delete/:id', async function (req, res) {
-
-//   if (!ObjectId.isValid(req.params.id))
-//   return res.status(404).send('Unable to find the requested resource!');
-
-//   let result = await db.collection("bookings").findOneAndDelete({ _id: ObjectId(req.params.id) })
-  
-//   if (!result) return res.status(404).send('Unable to find the requested resource!');
-
-//   res.render("delete_redirect", {booking: result});
-
-// });
+ /* Handle the Form */
+router.post('/insert_gift', async function (req, res) {
+  req.body.type = "Gift"
+  let result = await db.collection("ngoData").insertOne(req.body);
+  res.status(201).json({ text: 'Created' });
+ });
 
 /* Ajax-Pagination */
-router.get('/api/bookings', async function (req, res) {
+router.get('/api/inventory', async function (req, res) {
 
-  var perPage = Math.max(req.query.perPage, 2) || 2;
+  var whereClause = {};
+  if(req.query.type) whereClause.type = req.query.type;
+  if (req.query.title) whereClause.title = { $regex: req.query.title };
 
-  var results = await db.collection("ngoData").find({}, {
+  var perPage = Math.max(req.query.perPage, 6) || 6;
+
+  var results = await db.collection("ngoData").find(whereClause, {
     limit: perPage,
     skip: perPage * (Math.max(req.query.page - 1, 0) || 0)
   }).toArray();
 
-  var pages = Math.ceil(await db.collection("ngoData").count() / perPage);
-
-  // return res.render('paginate', { bookings: results, pages: pages, perPage: perPage });
+  var pages = Math.ceil(await db.collection("ngoData").count(whereClause) / perPage);
 
   return res.json({ ngoData: results, pages: pages })
+
+});
+
+router.post('/api/insert/inventory', async function (req, res) {
+
+  let result = await db.collection("ngoData").insertOne(req.body);
+  res.status(201).json({text: result.insertedId});  
+});
+
+// Form for updating a single Booking 
+router.get('/api/inventory/detail/:id', async function (req, res) {
+
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(404).send('Unable to find the requested resource!');
+
+  let result = await db.collection("ngoData").findOne({ _id: ObjectId(req.params.id) });
+
+  if (!result) return res.status(404).send('Unable to find the requested resource!');
+
+  res.json(result);
+
+});
+
+// Updating a single Booking - Ajax
+router.put('/api/update/:id', async function (req, res) {
+
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(404).send('Unable to find the requested resource!');
+
+  var result = await db.collection("ngoData").findOneAndReplace(
+    { _id: ObjectId(req.params.id) }, req.body
+  );
+
+  if (!result.value)
+    return res.status(404).send('Unable to find the requested resource!');
+
+  res.send("Inventory updated.");
+
+});
+
+// Delete a single Booking - Ajax
+router.delete('/api/delete/:id', async function (req, res) {
+
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(404).send('Unable to find the requested resource!');
+
+  var result = await db.collection("ngoData").findOneAndDelete({ _id: ObjectId(req.params.id) });
+
+  if (!result.value)
+    return res.status(404).send('Unable to find the requested resource!');
+  res.send("Inventory deleted.");
+
+});
+
+// GroupBy
+router.get('/api/inventory/aggregate/groupby', async function (req, res) {
+
+  const pipeline = [
+    { $group: { _id: "$type", count: { $sum: 1 } } }
+  ];
+
+  const results = await db.collection("ngoData").aggregate(pipeline).toArray();
+
+  return res.json(results);
 
 });
 
